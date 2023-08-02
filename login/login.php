37d4a077237ad922d3ed165c469791f985ac083b
siMobin <shakibulislammobin@gmail.com>
2023-08-02 19:46:22 +0600

    update

--- login/login.php
+++ login/login.php
@@ -1,14 +1,14 @@
 <?php
 //connect to sql server using windows authentication
 $serverName = "ACER_LAPTOP\SQLEXPRESS"; //serverName\instanceName
-$connectionInfo = array( "Database"=>"airline");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+$connectionInfo = array("Database" => "airline");
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-if( $conn ) {
-     echo "Connection established.<br />";
-}else{
-     echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
+if ($conn) {
+    // echo "Connection established.<br />";
+} else {
+    echo "Connection could not be established.<br />";
+    die(print_r(sqlsrv_errors(), true));
 }
 
 //check if the user is already logged in sent him a logout options
@@ -19,78 +19,78 @@ if( $conn ) {
 //     exit;
 // }
 
-session_start();//check if the user is already logged in
-if(isset($_SESSION["username"])){
+session_start(); //check if the user is already logged in
+if (isset($_SESSION["username"])) {
     // User is logged in, redirect to the home page
     header("Location: ../home.php");
     exit;
 }
 
 //check if the user submitted the login form
-if(isset($_POST["login"])){ 
+if (isset($_POST["login"])) {
     //get the username and password from the form
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     //validate the input
-    if(empty($username) || empty($password)){
-        echo "Please enter both username and password.<br />";
-    }else{
+    if (empty($username) || empty($password)) {
+        echo "<p class='report'>Please enter both username and password.</p> ";
+    } else {
         //prepare and execute the sql query to check if the user exists in the database
         $sql = "SELECT * FROM users WHERE username = ?";
         $params = array($username);
         $stmt = sqlsrv_query($conn, $sql, $params);
-        if($stmt === false){
-            die( print_r( sqlsrv_errors(), true));
+        if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true));
         }
 
         //fetch the result
         $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-        if($row){
+        if ($row) {
             //verify the password
-            if(password_verify($password, $row["password"])){
+            if (password_verify($password, $row["password"])) {
                 //set the session variable and redirect to the home page
                 $_SESSION["username"] = $username;
                 header("Location: ../home.php");
                 exit;
-            }else{
-                echo "Incorrect password.<br />";
+            } else {
+                echo "<p class='report'>Incorrect password.</p> ";
             }
-        }else{
-            echo "User not found.<br />";
+        } else {
+            echo "<p class='report'>User not found.</p> ";
         }
     }
 }
 
 //check if the user submitted the registration form
-if(isset($_POST["register"])){
+if (isset($_POST["register"])) {
     //get the username, email and password from the form
     $username = $_POST["username"];
     $email = $_POST["email"];
     $password = $_POST["password"];
 
     //validate the input
-    if(empty($username) || empty($email) || empty($password)){
-        echo "Please enter all the required fields.<br />";
-    }else{
+    if (empty($username) || empty($email) || empty($password)) {
+        echo "<p class='report'>Please enter all the required fields.</p> ";
+    } else {
         //prepare and execute the sql query to check if the username or email already exists in the database
         $sql = "SELECT * FROM users WHERE username = ? OR email = ?";
         $params = array($username, $email);
         $stmt = sqlsrv_query($conn, $sql, $params);
-        if($stmt === false){
-            die( print_r( sqlsrv_errors(), true));
+        if ($stmt === false) {
+            die(print_r(sqlsrv_errors(), true));
         }
 
         //fetch the result
         $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-        if($row){
+        if ($row) {
             //check which field is already taken
-            if($row["username"] == $username){
-                echo "Username already taken.<br />";
-            }else{
-                echo "Email already taken.<br />";
+            if ($row["username"] == $username) {
+                echo "<p class='report'>Username already taken.</p> ";
+            } else {
+                echo "<p class='report'>Email already taken.</p> ";
             }
-        }else{
+        } else {
             //hash the password
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
@@ -98,8 +98,8 @@ if(isset($_POST["register"])){
             $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
             $params = array($username, $email, $hashed_password);
             $stmt = sqlsrv_query($conn, $sql, $params);
-            if($stmt === false){
-                die( print_r( sqlsrv_errors(), true));
+            if ($stmt === false) {
+                die(print_r(sqlsrv_errors(), true));
             }
 
             //set the session variable and redirect to the home page
@@ -112,23 +112,41 @@ if(isset($_POST["register"])){
 ?>
 
 <html>
+
 <head>
-<title>Login/Registration Form</title>
+    <link rel="stylesheet" href="login.css">
+    <title>Login/Registration Form</title>
 </head>
+
 <body>
-<h1>Login/Registration Form</h1>
-<h2>Login</h2>
-<form method="post" action="login.php">
-Username: <input type="text" name="username"><br />
-Password: <input type="password" name="password"><br />
-<input type="submit" name="login" value="Login">
-</form>
-<h2>Register</h2>
-<form method="post" action="login.php">
-Username: <input type="text" name="username"><br />
-Email: <input type="email" name="email"><br />
-Password: <input type="password" name="password"><br />
-<input type="submit" name="register" value="Register">
-</form>
+    <a href="../home.php"><i class="fa-solid fa-plane-lock"> private jet</i></a>
+    <div id="form">
+        <div class="login">
+            <h2>Login</h2>
+            <hr>
+            <form method="post" action="login.php">
+                <h6>Username:</h6>
+                <input type="text" name="username" placeholder="User Name">
+                <h6>Password:</h6>
+                <input type="password" name="password" placeholder="password"><br>
+                <input class="submit" type="submit" name="login" value="Login">
+            </form>
+        </div>
+        <hr>
+        <div class="reg">
+            <h2>Register</h2>
+            <hr>
+            <form method="post" action="login.php">
+                <h6>Username:</h6>
+                <input type="text" name="username" placeholder="User Name"><br />
+                <h6> Email:</h6>
+                <input type="email" name="email" placeholder="you@gmail.com"><br />
+                <h6>Password:</h6>
+                <input type="password" name="password" placeholder="password"><br />
+                <input class="submit" type="submit" name="register" value="Register">
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
--- ticket/ticket.php
+++ ticket/ticket.php
@@ -85,71 +85,71 @@ if (isset($_POST['submit'])) {
                     // Generate PDF
                     require('./fpdf/fpdf.php');
 
-                    $pdf = new FPDF('p','mm','A4');
+                    $pdf = new FPDF('p', 'mm', 'A4');
                     $pdf->AddPage();
                     // ticket background
-                    $pdf->Image('../image/ticket.png',-10,-5,230);
+                    $pdf->Image('../image/ticket.png', -10, -5, 230);
                     $pdf->SetFont('Arial', 'BU', 24);
-                    $pdf->Cell(71, 10, '',0,0);
-                    $pdf->Cell(59, 5, 'Privet Jet!',0,0);
-                    $pdf->Cell(59, 10, '',0,0);
+                    $pdf->Cell(71, 10, '', 0, 0);
+                    $pdf->Cell(59, 5, 'Privet Jet!', 0, 0);
+                    $pdf->Cell(59, 10, '', 0, 0);
                     // $pdf->Ln();
                     $pdf->SetFont('Arial', 'B', 12);
 
-// Display ID
-if (isset($id)) {
-    // Display ID
-    $pdf->Ln();
-    $pdf->Cell(10, 10, 'SID: ');
-    $pdf->SetFont('Arial', 'I', 12);
-    $pdf->Cell(140, 10, ' ' . $id);
-}
-$pdf->Ln();
+                    // Display ID
+                    if (isset($id)) {
+                        // Display ID
+                        $pdf->Ln();
+                        $pdf->Cell(10, 10, 'SID: ');
+                        $pdf->SetFont('Arial', 'I', 12);
+                        $pdf->Cell(140, 10, ' ' . $id);
+                    }
+                    $pdf->Ln();
 
 
-                // from
-                $pdf->SetFont('Arial', 'B', 12);
-                $pdf->Cell(10, 10, 'From:');
-                $pdf->SetFont('Arial', 'I', 12);
-                $pdf->Cell(1, 10, '   ' . htmlspecialchars($fromName));
-                $pdf->Ln();
-// to
-$pdf->SetFont('Arial', 'B', 12);
-$pdf->Cell(10, 10, 'To:');
-$pdf->SetFont('Arial', 'I', 12);
-$pdf->Cell(1, 10, '   ' . htmlspecialchars($toName));
+                    // from
+                    $pdf->SetFont('Arial', 'B', 12);
+                    $pdf->Cell(10, 10, 'From:');
+                    $pdf->SetFont('Arial', 'I', 12);
+                    $pdf->Cell(1, 10, '   ' . htmlspecialchars($fromName));
+                    $pdf->Ln();
+                    // to
+                    $pdf->SetFont('Arial', 'B', 12);
+                    $pdf->Cell(10, 10, 'To:');
+                    $pdf->SetFont('Arial', 'I', 12);
+                    $pdf->Cell(1, 10, '   ' . htmlspecialchars($toName));
 
-$pdf->Ln();
-// $pdf->Ln();          
-$pdf->Cell(95, 10, 'Travel Date: ' . htmlspecialchars($date->format('Y-m-d')),1,0,'C');
-// Display return date
-if ($trip == 'round-trip') {
-    $pdf->Cell(95, 10, 'Return Date: ' . htmlspecialchars($returnDate->format('Y-m-d')),1,0,'C');
-}
+                    $pdf->Ln();
+                    // $pdf->Ln();          
+                    $pdf->Cell(95, 10, 'Travel Date: ' . htmlspecialchars($date->format('Y-m-d')), 1, 0, 'C');
+                    // Display return date
+                    if ($trip == 'round-trip') {
+                        $pdf->Cell(95, 10, 'Return Date: ' . htmlspecialchars($returnDate->format('Y-m-d')), 1, 0, 'C');
+                    }
 
                     // Display class and cost
                     if (isset($class)) {
                         // Display class
                         $pdf->Ln();
                         // Display class
-                    $pdf->SetFont('Arial', 'B', 12);
-                    $pdf->Cell(15, 10, 'Class:');
-                    $pdf->SetFont('Arial', 'I', 12);
-                    $pdf->Cell(40, 10, ' ' . htmlspecialchars($class));
+                        $pdf->SetFont('Arial', 'B', 12);
+                        $pdf->Cell(15, 10, 'Class:');
+                        $pdf->SetFont('Arial', 'I', 12);
+                        $pdf->Cell(40, 10, ' ' . htmlspecialchars($class));
                     }
 
+                    // Display passengers
+                    if (isset($passengers)) {
                         // Display passengers
-                        if (isset($passengers)) {
-                           // Display passengers
-                    // $pdf->Ln();
-                    $pdf->SetFont('Arial', 'B', 12);
-                    $pdf->Cell(25, 10, 'Passengers: ');
-                    $pdf->SetFont('Arial', 'I', 12);
-                    $pdf->Cell(40, 10, ' ' . htmlspecialchars($passengers));
-                        }
+                        // $pdf->Ln();
+                        $pdf->SetFont('Arial', 'B', 12);
+                        $pdf->Cell(25, 10, 'Passengers: ');
+                        $pdf->SetFont('Arial', 'I', 12);
+                        $pdf->Cell(40, 10, ' ' . htmlspecialchars($passengers));
+                    }
 
-                       // Display phone
-                       if (isset($phone)) {
+                    // Display phone
+                    if (isset($phone)) {
                         // Display phone
                         $pdf->Ln();
                         $pdf->SetFont('Arial', 'B', 12);
@@ -160,13 +160,13 @@ if ($trip == 'round-trip') {
 
                     // Display cost
                     if (isset($cost)) {
-                        $pdf->SetTextColor(255,0,0);
-                    // Display cost
-                    $pdf->Ln();
-                    $pdf->SetFont('Arial', 'B', 12);
-                    $pdf->Cell(175, 10, 'Cost:  ',0,0,'R');
-                    $pdf->SetFont('Arial', 'I', 12);
-                    $pdf->Cell(10, 10, '  $' . htmlspecialchars($cost),0,1,'R');
+                        $pdf->SetTextColor(255, 0, 0);
+                        // Display cost
+                        $pdf->Ln();
+                        $pdf->SetFont('Arial', 'B', 12);
+                        $pdf->Cell(175, 10, 'Cost:  ', 0, 0, 'R');
+                        $pdf->SetFont('Arial', 'I', 12);
+                        $pdf->Cell(10, 10, '  $' . htmlspecialchars($cost), 0, 1, 'R');
                     }
 
                     // Output PDF
@@ -211,42 +211,42 @@ if ($trip == 'round-trip') {
             } else {
                 // Generate PDF
                 require('./fpdf/fpdf.php');
-                $pdf = new FPDF('p','mm','A4');
+                $pdf = new FPDF('p', 'mm', 'A4');
                 $pdf->AddPage();
                 // ticket background
-                $pdf->Image('../image/ticket.png',-10,-5,230);
+                $pdf->Image('../image/ticket.png', -10, -5, 230);
                 $pdf->SetFont('Arial', 'BU', 24);
-                $pdf->Cell(71, 10, '',0,0);
-                $pdf->Cell(59, 5, 'Privet Jet!',0,0);
-                $pdf->Cell(59, 10, '',0,0);
+                $pdf->Cell(71, 10, '', 0, 0);
+                $pdf->Cell(59, 5, 'Privet Jet!', 0, 0);
+                $pdf->Cell(59, 10, '', 0, 0);
                 $pdf->Ln();
                 $pdf->SetFont('Arial', 'B', 12);
 
-// Display ID
-if (isset($id)) {
-    // Display ID
-    $pdf->Ln();
-    $pdf->Cell(10, 10, 'SID: ');
-    $pdf->SetFont('Arial', 'I', 12);
-    $pdf->Cell(140, 10, ' ' . $id);
-    $pdf->Cell(40, 10, 'Travel Date: ' . htmlspecialchars($date->format('Y-m-d')));
-}
+                // Display ID
+                if (isset($id)) {
+                    // Display ID
+                    $pdf->Ln();
+                    $pdf->Cell(10, 10, 'SID: ');
+                    $pdf->SetFont('Arial', 'I', 12);
+                    $pdf->Cell(140, 10, ' ' . $id);
+                    $pdf->Cell(40, 10, 'Travel Date: ' . htmlspecialchars($date->format('Y-m-d')));
+                }
 
-$pdf->Ln();
+                $pdf->Ln();
                 // from
                 $pdf->SetFont('Arial', 'B', 12);
                 $pdf->Cell(10, 10, 'From:');
                 $pdf->SetFont('Arial', 'I', 12);
                 $pdf->Cell(1, 10, '   ' . htmlspecialchars($fromName));
                 $pdf->Ln();
-// to
-$pdf->SetFont('Arial', 'B', 12);
-$pdf->Cell(10, 10, 'To:');
-$pdf->SetFont('Arial', 'I', 12);
-$pdf->Cell(1, 10, '   ' . htmlspecialchars($toName));
+                // to
+                $pdf->SetFont('Arial', 'B', 12);
+                $pdf->Cell(10, 10, 'To:');
+                $pdf->SetFont('Arial', 'I', 12);
+                $pdf->Cell(1, 10, '   ' . htmlspecialchars($toName));
 
                 $pdf->Ln();
-                
+
                 // Display class and cost
                 if (isset($class)) {
                     // Display class
@@ -256,8 +256,8 @@ $pdf->Cell(1, 10, '   ' . htmlspecialchars($toName));
                     $pdf->Cell(40, 10, ' ' . htmlspecialchars($class));
                 }
 
-                 // Display passengers
-                 if (isset($passengers)) {
+                // Display passengers
+                if (isset($passengers)) {
                     // Display passengers
                     // $pdf->Ln();
                     $pdf->SetFont('Arial', 'B', 12);
@@ -265,25 +265,25 @@ $pdf->Cell(1, 10, '   ' . htmlspecialchars($toName));
                     $pdf->SetFont('Arial', 'I', 12);
                     $pdf->Cell(40, 10, ' ' . htmlspecialchars($passengers));
                 }
+                // Display phone
+                if (isset($phone)) {
                     // Display phone
-                    if (isset($phone)) {
-                        // Display phone
-                        $pdf->Ln();
-                        $pdf->SetFont('Arial', 'B', 12);
-                        $pdf->Cell(15, 10, 'Phone: ');
-                        $pdf->SetFont('Arial', 'I', 12);
-                        $pdf->Cell(40, 10, ' ' . htmlspecialchars($phone));
-                    }
+                    $pdf->Ln();
+                    $pdf->SetFont('Arial', 'B', 12);
+                    $pdf->Cell(15, 10, 'Phone: ');
+                    $pdf->SetFont('Arial', 'I', 12);
+                    $pdf->Cell(40, 10, ' ' . htmlspecialchars($phone));
+                }
 
                 // Display cost
                 if (isset($cost)) {
-                    $pdf->SetTextColor(255,0,0);
+                    $pdf->SetTextColor(255, 0, 0);
                     // Display cost
                     $pdf->Ln();
                     $pdf->SetFont('Arial', 'B', 12);
-                    $pdf->Cell(175, 10, 'Cost:  ',0,0,'R');
+                    $pdf->Cell(175, 10, 'Cost:  ', 0, 0, 'R');
                     $pdf->SetFont('Arial', 'I', 12);
-                    $pdf->Cell(10, 10, '  $' . htmlspecialchars($cost),0,1,'R');
+                    $pdf->Cell(10, 10, '  $' . htmlspecialchars($cost), 0, 1, 'R');
                 }
 
                 // Output PDF
@@ -314,110 +314,114 @@ while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="ticket.css">
     <title>ticket</title>
 </head>
+
 <body>
     <!-- include nav bar!!!!! -->
-<?php include '../partials/nav.php'; ?>
-<header>
-    <div>
-    <h1>YOUR PRIVET AIRLINE!</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam culpa iste consectetur aliquid debitis esse asperiores inventore hic? Nam, veritatis!</p></div>
-</header>
-<form method="post">
-<div class="border">
-    <!-- //////////////////////// -->
-    <div class="destination">
-    <div class="box">
-    <label for="from">From:</label>
-    <select id="from" name="from">
-        <?php foreach ($locations as $id => $destination): ?>
-            <option value="<?php echo htmlspecialchars($id); ?>"><?php echo htmlspecialchars($destination); ?></option>
-        <?php endforeach; ?>
-    </select>
-    </div>
+    <?php include '../partials/nav.php'; ?>
+    <header>
+        <div>
+            <h1>YOUR PRIVET AIRLINE!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam culpa iste consectetur aliquid debitis esse asperiores inventore hic? Nam, veritatis!</p>
+        </div>
+    </header>
+    <form method="post">
+        <div class="border">
+            <!-- //////////////////////// -->
+            <div class="destination">
+                <div class="box">
+                    <label for="from">From:</label>
+                    <select id="from" name="from">
+                        <?php foreach ($locations as $id => $destination) : ?>
+                            <option value="<?php echo htmlspecialchars($id); ?>"><?php echo htmlspecialchars($destination); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
 
-<div  class="box">
-    <label for="to">To:</label>
-    <select id="to" name="to">
-        <?php foreach ($locations as $id => $destination): ?>
-            <option value="<?php echo htmlspecialchars($id); ?>"><?php echo htmlspecialchars($destination); ?></option>
-        <?php endforeach; ?>
-    </select>
-    </div>
-    
-    <!-- /////////// -->
+                <div class="box">
+                    <label for="to">To:</label>
+                    <select id="to" name="to">
+                        <?php foreach ($locations as $id => $destination) : ?>
+                            <option value="<?php echo htmlspecialchars($id); ?>"><?php echo htmlspecialchars($destination); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
 
-    <div class="date">
-    <div  class="box">
-    <label for="trip">Trip:</label>
-    <select id="trip" name="trip">
-        <option value="one-way">One-way</option>
-        <option value="round-trip">Round-trip</option>
-    </select>
-    </div>
-    <div class="travel_date box">
-    <label for="date">Date:</label>
-    <input type="date" id="date" name="date">
-    </div>
+                <!-- /////////// -->
 
-    <!-- Add return date input -->
-    <div  class="box" id="return-date-input" style="display: none;">
-        <label for="return-date">Return Date:</label><br>
-        <input type="date" id="return-date" name="return-date"> 
-    </div>
-    </div>
-    </div>
+                <div class="date">
+                    <div class="box">
+                        <label for="trip">Trip:</label>
+                        <select id="trip" name="trip">
+                            <option value="one-way">One-way</option>
+                            <option value="round-trip">Round-trip</option>
+                        </select>
+                    </div>
+                    <div class="travel_date box">
+                        <label for="date">Date:</label>
+                        <input type="date" id="date" name="date">
+                    </div>
 
-    <div class="more_info">
+                    <!-- Add return date input -->
+                    <div class="box" id="return-date-input" style="display: none;">
+                        <label for="return-date">Return Date:</label><br>
+                        <input type="date" id="return-date" name="return-date">
+                    </div>
+                </div>
+            </div>
 
-    <!-- <div class="seat"> -->
-    <div class="quality box_2" >
-    <label for="class">Class:</label>
-    <select id="class" name="class">
-        <option value="economy">Economy</option>
-        <option value="business">Business</option>
-        <option value="first">First</option>
-    </select> 
-    </div>
+            <div class="more_info">
 
-    <div class="box_2">
-    <label for="passengers">Passengers:</label>
-    <input type="number" id="passengers" name="passengers"> 
-    </div>
-    <!-- </div> -->
+                <!-- <div class="seat"> -->
+                <div class="quality box_2">
+                    <label for="class">Class:</label>
+                    <select id="class" name="class">
+                        <option value="economy">Economy</option>
+                        <option value="business">Business</option>
+                        <option value="first">First</option>
+                    </select>
+                </div>
 
-    <!-- Add email and phone inputs -->
-    <!-- <div class="contact"> -->
-    <div class="box_2">
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email"> 
-    </div>
+                <div class="box_2">
+                    <label for="passengers">Passengers:</label>
+                    <input type="number" id="passengers" name="passengers">
+                </div>
+                <!-- </div> -->
 
-    <div class="box_2">
-    <label for="phone">Phone:</label>
-    <input type="tel" id="phone" name="phone">
-    </div>
-    </div>
-    </div>
-    <!-- </div> -->
+                <!-- Add email and phone inputs -->
+                <!-- <div class="contact"> -->
+                <div class="box_2">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email">
+                </div>
 
-    <input class="submit" type="submit" name="submit" value="Submit">
-</form>
+                <div class="box_2">
+                    <label for="phone">Phone:</label>
+                    <input type="tel" id="phone" name="phone">
+                </div>
+            </div>
+        </div>
+        <!-- </div> -->
 
-<script>
-    // Show/hide return date input based on trip selection
-    document.getElementById('trip').addEventListener('change', function () {
-        if (this.value === 'round-trip') {
-            document.getElementById('return-date-input').style.display = 'block';
-        } else {
-            document.getElementById('return-date-input').style.display = 'none';
-        }
-    });
-</script>
+        <input class="submit" type="submit" name="submit" value="Submit">
+    </form>
+
+    <script>
+        // Show/hide return date input based on trip selection
+        document.getElementById('trip').addEventListener('change', function() {
+            if (this.value === 'round-trip') {
+                document.getElementById('return-date-input').style.display = 'block';
+            } else {
+                document.getElementById('return-date-input').style.display = 'none';
+            }
+        });
+    </script>
 </body>
+
 </html>
