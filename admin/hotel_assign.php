ca194abfda0186d5159ad355a90849030ccfe86c
siMobin <shakibulislammobin@gmail.com>
2023-08-21 20:25:11 +0600

    upload hotel management

---
+++ admin/hotel_assign.php
@@ -0,0 +1,206 @@
+<?php require_once('./nav.php'); ?>
+<?php require_once('./conn.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/hotel_assign.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="block">
+        <?php
+        function compareDates($a, $b)
+        {
+            return strcmp($a['date'], $b['date']);
+        }
+        // Function to check if a booking ID exists in the hotel_assign table
+        function bookingExistsInHotelAssign($booking_id, $conn)
+        {
+            $sql = "SELECT COUNT(*) AS count FROM hotel_assign WHERE id = ?";
+            $params = array($booking_id);
+
+            $stmt = sqlsrv_query($conn, $sql, $params);
+            if ($stmt === false) {
+                die(print_r(sqlsrv_errors(), true));
+            }
+
+            $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+            return $row['count'] > 0;
+        }
+
+
+        // Retrieve booking IDs and corresponding check-in dates
+        $bookings = array();
+        $sql = "SELECT id, date FROM flight_assign";
+        $result = sqlsrv_query($conn, $sql);
+        while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+            $bookings[] = array(
+                'id' => $row["id"],
+                'date' => $row["date"]->format('Y-m-d')
+            );
+        }
+        usort($bookings, 'compareDates');
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            foreach ($_POST as $key => $value) {
+                if (strpos($key, "checkout_date_") === 0) {
+                    $id = substr($key, strlen("checkout_date_"));
+                    $checkout_date = new DateTime($value);
+                    $checkout_date_string = $checkout_date->format('Y-m-d');
+                    $roles = array("pilot", "co_pilot", "hostess", "co_hostess", "co_hostess_secondary");
+
+                    foreach ($roles as $role) {
+                        $room_no = $_POST["{$role}_room_$id"];
+                        $sql = "INSERT INTO hotel_assign (id, did, roll, checkin_date, checkout_date, room_no, hotel)
+                            VALUES (?, ?, ?, ?, ?, ?, ?)";
+                        $params = array(
+                            $id,
+                            getDriverDID($id, $role, $conn),
+                            $role,
+                            getCheckinDate($id, $conn),
+                            $checkout_date_string,
+                            $room_no,
+                            $_POST["hotel_name_$id"] // Use the posted hotel name
+                        );
+
+                        $stmt = sqlsrv_query($conn, $sql, $params);
+
+                        if (!$stmt) {
+                            die(print_r(sqlsrv_errors(), true));
+                        }
+                    }
+                }
+            }
+            // Redirect back to the form page with a success message
+            header("Location: hotel_assign.php?success=!");
+            exit();
+        }
+
+        // Function to retrieve the check-in date for a given booking ID
+        function getCheckinDate($booking_id, $conn)
+        {
+            $sql = "SELECT date FROM flight_assign WHERE id = ?";
+            $params = array($booking_id);
+
+            $stmt = sqlsrv_query($conn, $sql, $params);
+            if ($stmt === false) {
+                die(print_r(sqlsrv_errors(), true));
+            }
+
+            $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+            $checkin_date = $row["date"];
+
+            return $checkin_date;
+        }
+
+        // Function to retrieve the "to" location name for a given booking ID
+        function getToLocation($booking_id, $conn)
+        {
+            $sql = "SELECT [to] FROM bookings WHERE id = ?";
+            $params = array($booking_id);
+
+            $stmt = sqlsrv_query($conn, $sql, $params);
+            if ($stmt === false) {
+                die(print_r(sqlsrv_errors(), true));
+            }
+
+            $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+            $to_location = $row["to"];
+
+            return $to_location;
+        }
+
+        // Function to retrieve the corresponding DID for a driver role
+        function getDriverDID($booking_id, $driver_role, $conn)
+        {
+            $sql = "SELECT $driver_role FROM flight_assign WHERE id = ?";
+            $params = array($booking_id);
+
+            $stmt = sqlsrv_query($conn, $sql, $params);
+            if ($stmt === false) {
+                die(print_r(sqlsrv_errors(), true));
+            }
+
+            $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+            return $row[$driver_role];
+        }
+
+        // Function to retrieve the hotel name for a given booking ID
+        function getHotelForBooking($booking_id, $conn)
+        {
+            $sql = "SELECT [to] FROM bookings WHERE id = ?";
+            $params = array($booking_id);
+
+            $stmt = sqlsrv_query($conn, $sql, $params);
+            if ($stmt === false) {
+                die(print_r(sqlsrv_errors(), true));
+            }
+
+            $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+            $destination = $row["to"];
+
+            return getHotelNameForDestination($destination, $conn);
+        }
+
+        // Function to retrieve the hotel name for a given destination
+        function getHotelNameForDestination($destination, $conn)
+        {
+            $sql = "SELECT hotel FROM locations WHERE destination = ?";
+            $params = array($destination);
+
+            $stmt = sqlsrv_query($conn, $sql, $params);
+            if ($stmt === false) {
+                die(print_r(sqlsrv_errors(), true));
+            }
+
+            $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+            return $row["hotel"];
+        }
+
+        // Generate HTML content for each booking
+        foreach ($bookings as $booking) {
+            $id = $booking['id'];
+            // Skip booking ID if it already exists in hotel_assign
+            if (bookingExistsInHotelAssign($id, $conn)) {
+                continue;
+            }
+
+            $hotel_name = getHotelForBooking($id, $conn);
+            $to_location = getToLocation($id, $conn);
+            echo "<div class='wrap'>";
+            echo "<h2>Booking ID: $id</h2>
+                  <form method='POST'>
+                    <p>Airport Location: $to_location</p>
+                    <p>Check-in Date: {$booking['date']}</p>";
+
+            $roles = array("pilot", "co_pilot", "hostess", "co_hostess", "co_hostess_secondary");
+            foreach ($roles as $role) {
+                echo "<div class='box'>";
+                echo "<label for='{$role}_room_$id'>Room Number for $role:</label>
+                  <input type='text' name='{$role}_room_$id' placeholder='room no. for $role'>";
+                echo "</div>";
+            }
+
+            echo "<div class='box'>";
+            echo "<label for='hotel_name_$id'>Hotel Name:</label>
+             <input type='text' name='hotel_name_$id' value='$hotel_name' required>
+            </div>
+            <div class='box'>";
+            echo "<label for='checkout_date_$id'>Checkout Date:</label>
+              <input type='date' name='checkout_date_$id' required>
+            </div>
+            <button class='submit' type='submit' name='assign_$id'>Assign</button>
+            </form>";
+            echo "</div>";
+        }
+        sqlsrv_close($conn);
+        ?>
+    </div>
+</body>
+
+</html>
--- admin/nav.php
+++ admin/nav.php
@@ -39,10 +39,54 @@ function logout()
         <!-- The navigation links -->
         <ul>
             <li><a href="./validation.php" <?php if (basename($_SERVER['PHP_SELF']) == 'validation.php') echo 'class="active"'; ?>>Validation</a></li>
-            <li><a href="./flight_assign.php" <?php if (basename($_SERVER['PHP_SELF']) == 'flight_assign.php') echo 'class="active"'; ?>>Flight Assignment</a></li>
+            <!-- assignment -->
+            <li class="dropdown">
+                <a href="javascript:void(0);" class="dropbtn">
+                    <?php
+                    $assignmentActive = basename($_SERVER['PHP_SELF']) == 'flight_assign.php' || basename($_SERVER['PHP_SELF']) == 'hotel_assign.php';
+                    if ($assignmentActive) {
+                        if (basename($_SERVER['PHP_SELF']) == 'flight_assign.php') {
+                            echo "<span class='active'>Flight Assignment<span>";
+                        } elseif (basename($_SERVER['PHP_SELF']) == 'hotel_assign.php') {
+                            echo "<span class='active'>Hotel Assignment<span>";
+                        }
+                    } else {
+                        echo 'Assignment';
+                    }
+                    ?>
+                    <i class="fa-solid fa-caret-down"></i>
+                </a>
+                <div class="dropdown-content">
+                    <a href="./flight_assign.php" <?php if (basename($_SERVER['PHP_SELF']) == 'flight_assign.php') echo 'class="active"'; ?>>Flight Assignment</a>
+                    <a href="./hotel_assign.php" <?php if (basename($_SERVER['PHP_SELF']) == 'hotel_assign.php') echo 'class="active"'; ?>>Hotel Assignment</a>
+                </div>
+            </li>
+            <!-- end of assignment -->
+
+            <!-- driver and airport -->
+            <li class="dropdown">
+                <a href="javascript:void(0);" class="dropbtn">
+                    <?php
+                    $assignmentActive = basename($_SERVER['PHP_SELF']) == 'drivers.php' || basename($_SERVER['PHP_SELF']) == 'locations.php';
+                    if ($assignmentActive) {
+                        if (basename($_SERVER['PHP_SELF']) == 'drivers.php') {
+                            echo "<span class='active'>Drivers<span>";
+                        } elseif (basename($_SERVER['PHP_SELF']) == 'locations.php') {
+                            echo "<span class='active'>Airports<span>";
+                        }
+                    } else {
+                        echo 'Assignment';
+                    }
+                    ?>
+                    <i class="fa-solid fa-caret-down"></i>
+                </a>
+                <div class="dropdown-content">
+                    <a href="./drivers.php" <?php if (basename($_SERVER['PHP_SELF']) == 'drivers.php') echo 'class="active"'; ?>>Drivers</a>
+                    <a href="./locations.php" <?php if (basename($_SERVER['PHP_SELF']) == 'locations.php') echo 'class="active"'; ?>>Airports</a>
+                </div>
+            </li>
+            <!-- end of driver and airport -->
             <li><a href="./schedule.php" <?php if (basename($_SERVER['PHP_SELF']) == 'schedule.php') echo 'class="active"'; ?>>Flight Schedule</a></li>
-            <li><a href="./drivers.php" <?php if (basename($_SERVER['PHP_SELF']) == 'drivers.php') echo 'class="active"'; ?>>Drivers</a></li>
-            <li><a href="./locations.php" <?php if (basename($_SERVER['PHP_SELF']) == 'locations.php') echo 'class="active"'; ?>>Airport</a></li>
             <li><a href="./stats.php" <?php if (basename($_SERVER['PHP_SELF']) == 'stats.php') echo 'class="active"'; ?>>Stats</a></li>
             <li class="logout"> <a method="GET" href="?action=logout">Logout</a></li>
         </ul>
