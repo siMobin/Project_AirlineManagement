d3b16d182c3691dd97a1d7ce62c8c2bced7e45fd
Sptm7731 <whaddafukman@gmail.com>
2023-10-07 17:32:28 +0600

    Updated locations to include hotel and hotline

--- admin/locations.php
+++ admin/locations.php
@@ -8,9 +8,11 @@ if (isset($_POST['submit'])) {
     $destination = $_POST['destination'];
     $latitude = $_POST['latitude'];
     $longitude = $_POST['longitude'];
+    $hotel = $_POST['hotel'];
+    $hotline = $_POST['hotline'];
 
-    $tsql = "INSERT INTO locations (destination, latitude, longitude) VALUES (?, ?, ?)";
-    $params = array($destination, $latitude, $longitude);
+    $tsql = "INSERT INTO locations (destination, latitude, longitude,hotel,hotline) VALUES (?, ?, ?,?,?)";
+    $params = array($destination, $latitude, $longitude,$hotel,$hotline);
     $stmt = sqlsrv_query($conn, $tsql, $params);
 
     if ($stmt === false) {
@@ -42,12 +44,14 @@ if ($stmt === false) {
 echo "<h1>airport list/Destinations</h1>";
 echo "<div class='table'>";
 echo "<table>";
-echo "<tr><th>Destination</th><th>Latitude</th><th>Longitude</th><th>Delete</th></tr>";
+echo "<tr><th>Destination</th><th>Latitude</th><th>Longitude</th><th>Hotel</th><th>Hotline</th><th>Delete</th></tr>";
 while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
     echo "<tr>";
     echo "<td>" . $row['destination'] . "</td>";
     echo "<td>" . $row['latitude'] . "</td>";
     echo "<td>" . $row['longitude'] . "</td>";
+    echo "<td>" . $row['hotel'] . "</td>";
+    echo "<td>" . $row['hotline'] . "</td>";
     // echo "<td><form method='post'><input type='hidden' name='id' value='" . $row['id'] . "'><input type='submit' name='delete' value='Delete'></form></td>";
     echo "<td><form method='post'><input type='hidden' name='id' value='" . $row['id'] . "'><button class='delete' type='submit' name='delete'><i class='fa fa-trash'></i></button></form></td>";
 
@@ -76,6 +80,10 @@ echo "</div>";
         <input type="text" id="latitude" name="latitude" required placeholder="d(N,S) + (m/60) + (s/3600)">
         <label for="longitude">Longitude:</label>
         <input type="text" id="longitude" name="longitude" required placeholder="d(E,W) + (m/60) + (s/3600)">
+        <label for="hotel">Hotel Name:</label>
+        <input type="text" id="hotel" name="hotel" required placeholder="hotel name">
+        <label for="hotline">Phone number:</label>
+        <input type="text" id="hotline" name="hotline" required placeholder="phone number">
         <input class="submit" type="submit" name="submit" value="Submit">
     </form>
     <?php include './map.php'; ?>
